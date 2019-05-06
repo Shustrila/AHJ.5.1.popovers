384d9cb00053a66f48daabdf39e15467979891d8
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-05-06 22:46:51 +0300

    fix: eslint

--- src/js/Popovers.js
+++ src/js/Popovers.js
@@ -1,70 +1,70 @@
 class Popovers {
-    constructor(obj) {
-        if (typeof obj !== "object") throw new TypeError('argument not object!');
-        if (obj.element === undefined) throw new TypeError('no root element!');
+  constructor(obj) {
+    if (typeof obj !== 'object') throw new TypeError('argument not object!');
+    if (obj.element === undefined) throw new TypeError('no root element!');
 
-        this.element = obj.element;
-        this.value = obj.value || 'button';
-        this.className = obj.className || 'popovers';
-        this.windowTitle = obj.windowTitle || 'the title';
-        this.windowDesc = obj.windowDesc || 'the description';
-        this.timeAnimation = obj.timeAnimation || 0.25;
+    this.element = obj.element;
+    this.value = obj.value || 'button';
+    this.className = obj.className || 'popovers';
+    this.windowTitle = obj.windowTitle || 'the title';
+    this.windowDesc = obj.windowDesc || 'the description';
+    this.timeAnimation = obj.timeAnimation || 0.25;
 
+    this.window = null;
+    this.button = null;
+
+    this.init();
+  }
+
+  init() {
+    const root = document.querySelector(this.element);
+    this.button = document.createElement('button');
+
+    this.button.className = this.className;
+    this.button.innerHTML = this.value;
+
+    this.button.addEventListener('click', e => this.onClickButton(e, root));
+
+    root.style.position = 'relative';
+    root.appendChild(this.button);
+  }
+
+  onClickButton(e, root) {
+    if (this.window === null) {
+      this.createWindow();
+      root.prepend(this.window);
+    } else {
+      this.window.style.opacity = 0;
+      setTimeout(() => {
+        this.window.remove();
         this.window = null;
-        this.button = null;
-
-        this.init();
+      }, this.timeAnimation * 1000);
     }
+  }
 
-    init() {
-        const root = document.querySelector(this.element);
-        this.button = document.createElement('button');
+  createWindow() {
+    this.window = document.createElement('div');
+    const title = document.createElement('h2');
+    const desc = document.createElement('p');
 
-        this.button.className = this.className;
-        this.button.innerHTML = this.value;
+    title.className = 'popover__window-title';
+    title.innerHTML = this.windowTitle;
 
-        this.button.addEventListener('click', (e) => this.onClickButton(e, root));
+    desc.className = 'popover__window-desc';
+    desc.innerHTML = this.windowDesc;
 
-        root.style.position = 'relative';
-        root.appendChild(this.button);
-    }
+    this.window.appendChild(title);
+    this.window.appendChild(desc);
+    this.window.className = 'popover__window';
+    // style
+    this.window.style.position = 'absolute';
+    this.window.style.opacity = 0;
+    this.window.style.left = '0px';
+    this.window.style.transform = 'translateY(-125%)';
+    this.window.style.transition = `${this.timeAnimation}s`;
 
-    onClickButton(e, root) {
-        if (this.window === null) {
-            this.createWindow();
-            root.prepend(this.window);
-        } else {
-            this.window.style.opacity = 0;
-            setTimeout(() => {
-                this.window.remove();
-                this.window = null;
-            }, this.timeAnimation * 1000);
-        }
-    }
-
-    createWindow() {
-        this.window = document.createElement('div');
-        const title = document.createElement('h2');
-        const desc = document.createElement('p');
-
-        title.className = 'popover__window-title';
-        title.innerHTML = this.windowTitle;
-
-        desc.className = 'popover__window-desc';
-        desc.innerHTML = this.windowDesc;
-
-        this.window.appendChild(title);
-        this.window.appendChild(desc);
-        this.window.className = 'popover__window';
-        // style
-        this.window.style.position = 'absolute';
-        this.window.style.opacity = 0;
-        this.window.style.left = '0px';
-        this.window.style.transform = 'translateY(-125%)';
-        this.window.style.transition = this.timeAnimation+'s';
-
-        setTimeout(() => this.window.style.opacity = 1, this.timeAnimation * 1000);
-    }
+    setTimeout(() => this.window.style.opacity = 1, this.timeAnimation * 1000);
+  }
 }
 
-export default Popovers
+export default Popovers;
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 import Popovers from './Popovers';
 
 new Popovers({
-    element: '[data-widget=popover]',
-    value: 'Click to toggle popover',
-    className: 'popovers popovers-button',
-    windowTitle: 'Popover title',
-    windowDesc: 'And here"s some amazing content. It"s very engaging. Right?'
+  element: '[data-widget=popover]',
+  value: 'Click to toggle popover',
+  className: 'popovers popovers-button',
+  windowTitle: 'Popover title',
+  windowDesc: 'And here"s some amazing content. It"s very engaging. Right?',
 });
-
